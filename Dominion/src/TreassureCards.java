dfcfa3a903eb512c68d317856a75f004471da1ab
YanniD <yanni.derous@student.howest.be>
2016-03-21 15:39:50 +0100

    Added files via upload

---
+++ Dominion/src/TreassureCards.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yanni
+ */
+public class TreassureCards {
+    private static final int COPPER = 1;
+    private static final int SILVER = 2;
+    private static final int GOLD = 3;
+    
+    
+}
