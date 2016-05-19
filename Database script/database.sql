a8ea5a0f72a0d818ec9b3dabe810c19ebbdf8fb6
MisterQ <quentin.bultinck@student.howest.be>
2016-05-19 08:35:55 +0200

    finished

--- Database script/database.sql
+++ Database script/database.sql
@@ -22,18 +22,18 @@ PRIMARY KEY(ID)
 /*firstGame*/
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(1 ,"cellar", 2, "C:\\School\\DominionFotos\\cellar.jpg",0,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(2 ,"market", 5, "C:\\School\\DominionFotos\\market.jpg",0,NULL,NULL,10);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(3 ,"militia", 4, "C:\\School\\DominionFotos\\militia.jpg",0,NULL,NULL,10);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(3 ,"militia", 4, "C:\\School\\DominionFotos\\militia.jpg",1,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(4 ,"mine", 5, "C:\\School\\DominionFotos\\mine.jpg",0,NULL,NULL,10);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(5 ,"moat", 2, "C:\\School\\DominionFotos\\moat.jpg",0,NULL,NULL,10);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(5 ,"moat", 2, "C:\\School\\DominionFotos\\moat.jpg",4,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(6 ,"remodel", 4, "C:\\School\\DominionFotos\\remodel.jpg",0,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(7 ,"smithy", 4, "C:\\School\\DominionFotos\\smithy.jpg",0,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(8 ,"village", 3, "C:\\School\\DominionFotos\\village.jpg",0,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(9 ,"woodcutter", 3, "C:\\School\\DominionFotos\\woodcutter.jpg",0,NULL,NULL,10);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(10 ,"workshop", 3, "C:\\School\\DominionFotos\\workshop.jpg",0,NULL,NULL,10);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(10,"workshop", 3, "C:\\School\\DominionFotos\\workshop.jpg",0,NULL,NULL,10);
 
 /*bigMoney*/
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(11 ,"adventurer", 6, "C:\\School\\DominionFotos\\adventurer.jpg",0,NULL,NULL,10);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(12 ,"bureaucrat", 4, "C:\\School\\DominionFotos\\bureaucrat.jpg",0,NULL,NULL,10);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(12 ,"bureaucrat", 4, "C:\\School\\DominionFotos\\bureaucrat.jpg",1,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(13 ,"chancellor", 3, "C:\\School\\DominionFotos\\chancellor.jpg",0,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(14 ,"chapel", 2, "C:\\School\\DominionFotos\\chapel.jpg",0,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(15 ,"feast", 4, "C:\\School\\DominionFotos\\feast.jpg",0,NULL,NULL,10);
@@ -45,23 +45,23 @@ INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VA
 /*interaction*/
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(19 ,"festival", 5, "C:\\School\\DominionFotos\\festival.jpg",0,NULL,NULL,10);
 INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(20 ,"library", 5, "C:\\School\\DominionFotos\\library.jpg",0,NULL,NULL,10);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(21 ,"spy", 4, "C:\\School\\DominionFotos\\spy.jpg",0,NULL,NULL,10);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(22 ,"thief", 4, "C:\\School\\DominionFotos\\thief.jpg",0,NULL,NULL,10);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(21 ,"spy", 4, "C:\\School\\DominionFotos\\spy.jpg",1,NULL,NULL,10);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(22 ,"thief", 4, "C:\\School\\DominionFotos\\thief.jpg",1,NULL,NULL,10);
 
 /*sizeDistortion*/
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(23 ,"gardens", 4, "C:\\School\\DominionFotos\\gardens.jpg",0,NULL,NULL,12);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(24 ,"witch", 5, "C:\\School\\DominionFotos\\witch.jpg",0,NULL,NULL,10);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(23 ,"gardens", 4, "C:\\School\\DominionFotos\\gardens.jpg",3,NULL,NULL,12);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(24 ,"witch", 5, "C:\\School\\DominionFotos\\witch.jpg",1,NULL,NULL,10);
 
 /*treasurecards*/
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(25 ,"copper", 0, "C:\\School\\DominionFotos\\copper.jpg",1,1,NULL,60);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(26 ,"silver", 3, "C:\\School\\DominionFotos\\silver.jpg",1,2,NULL,40);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(27 ,"gold", 6, "C:\\School\\DominionFotos\\gold.jpg",1,3,NULL,30);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(25 ,"copper", 0, "C:\\School\\DominionFotos\\copper.jpg",2,1,NULL,60);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(26 ,"silver", 3, "C:\\School\\DominionFotos\\silver.jpg",2,2,NULL,40);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(27 ,"gold", 6, "C:\\School\\DominionFotos\\gold.jpg",2,3,NULL,30);
 
 /*victorycards*/
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(28 ,"estate", 2, "C:\\School\\DominionFotos\\estate.jpg",2,NULL,1,24);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(29 ,"duchy", 5, "C:\\School\\DominionFotos\\duchy.jpg",2,NULL,3,12);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(30 ,"province", 8, "C:\\School\\DominionFotos\\province.jpg",2,NULL,6,12);
-INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(32 ,"curse",0,"C:\\School\\DominionFotos\\curse.jpg",2,NULL,-1,30);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(28 ,"estate", 2, "C:\\School\\DominionFotos\\estate.jpg",3,NULL,1,24);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(29 ,"duchy", 5, "C:\\School\\DominionFotos\\duchy.jpg",3,NULL,3,12);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(30 ,"province", 8, "C:\\School\\DominionFotos\\province.jpg",3,NULL,6,12);
+INSERT INTO cards(ID, NAME, Cost, Image,TYPE,Worth,VictoryPoints,StartAmount) VALUES(32 ,"curse",0,"C:\\School\\DominionFotos\\curse.jpg",3,NULL,-1,30);
 
 
 
